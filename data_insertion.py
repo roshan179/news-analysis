7c3aae91e300679a4bff3b38cb0a7e17718a9033
roshan179 <rk009.exe@gmail.com>
2025-03-06 20:34:06 +0530

    updated the script for data insertion as the root certificate was moved to a subfolder

--- data_insertion.py
+++ data_insertion.py
@@ -12,7 +12,7 @@ from web_scraping import fetch_news
 from datetime import datetime, timedelta
 from psycopg2.extras import execute_batch# 🟢 Store News Function (Now Using YugabyteDB)
 
-DB_URL = "postgresql://admin:xRZQdDx9_04APJdJc1CftnyqX9VZyX@ap-south-1.d67e1e29-cc8d-4b15-8cf9-4ea1e5bd8b9f.aws.yugabyte.cloud:5433/my_database?ssl=true&sslmode=verify-full&sslrootcert=./root.crt"
+DB_URL = "postgresql://admin:xRZQdDx9_04APJdJc1CftnyqX9VZyX@ap-south-1.d67e1e29-cc8d-4b15-8cf9-4ea1e5bd8b9f.aws.yugabyte.cloud:5433/my_database?ssl=true&sslmode=verify-full&sslrootcert=./Useful info/root.crt"
 
 def store_articles(articles):
     try:
